9d38a7104c2e8e4767982d217709dd61f7a53232
aselwilliams <aselwilliams22@gmail.com>
2022-05-19 15:59:45 -0400

    data displayed

--- src/Review.js
+++ src/Review.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react';
+import people from './data';
+import {FaChevronLeft, FaChevronRight, FaQuoteRight} from 'react-icons/fa';
 
 function Review() {
+    const [index,setIndex] = useState(0);
+    const {name, job, image, text} = people[index]
+    console.log(people)
   return (
-    <div>Review</div>
+    <article className='review'>
+        <div className='img-container'>
+            <img src={image} alt={name} className='person-img' />
+            <span className='quote-icon'>
+                <FaQuoteRight />
+            </span>
+        </div>
+        <h4 className='author'>{name}</h4>
+        <p className='job'>{job}</p>
+        <p className='info'>{text}
+        </p>
+    </article>
   )
 }
 
